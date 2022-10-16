2a4c88e107e17408ee7dfffd081a30d32cc3e9ea
arplaaay <86619106+arplaaay@users.noreply.github.com>
2022-10-16 15:47:52 +0000

    Deploying to gh-pages from @ arplaaay/goit-react-hw-05-movies@834cef376bb2c3c3a88fcb4fc60bf7b43ca20374 🚀

---
+++ static/js/520.1eefa74b.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[520],{520:function(e,r,t){t.r(r);var n=t(731),a=t(184);r.default=function(){return(0,a.jsxs)(a.Fragment,{children:[(0,a.jsx)("h1",{children:"404"}),(0,a.jsx)("h2",{children:"Page was not found"}),(0,a.jsx)("h3",{children:(0,a.jsx)(n.rU,{to:"/",children:"Back to Homepage"})})]})}}}]);
+//# sourceMappingURL=520.1eefa74b.chunk.js.map
